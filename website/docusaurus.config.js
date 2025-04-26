374ad3372e4a8cca6cd5389d1866f2fb8cb0511e
Wiktor Bajor <wiktorbajor1@gmail.com>
2025-04-26 22:10:01 +0200

    Format code and remove comments

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,45 +1,45 @@
 module.exports = {
-    title: 'Wiktor Bajor',
-    tagline: 'A personal website and blog',
-    url: 'https://wiktorbajor.com',
-    baseUrl: '/',
-    favicon: 'img/favicon.ico',
-    organizationName: 'Wiktor Bajor',
-    projectName: 'website',
+  title : 'Wiktor Bajor',
+  tagline : 'A personal website and blog',
+  url : 'https://wiktorbajor.com',
+  baseUrl : '/',
+  favicon : 'img/favicon.ico',
+  organizationName : 'Wiktor Bajor',
+  projectName : 'website',
 
-    themeConfig: {
-      navbar: {
-        title: 'Wiktor Bajor',
-        items: [
-          {
-            to: 'blog',
-            label: 'Blog',
-            position: 'left'
-          },
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Documentation',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} Wiktor Bajor. Built with Docusaurus.`,
-      },
+  themeConfig : {
+    navbar : {
+      title : 'Wiktor Bajor',
+      items :
+            [
+              {to : 'blog', label : 'Blog', position : 'left'},
+              {
+                type : 'doc',
+                docId : 'intro',
+                position : 'left',
+                label : 'Documentation',
+              },
+            ],
     },
-    presets: [
-      [
-        '@docusaurus/preset-classic',
-        {
-          docs: {
-            sidebarPath: require.resolve('./sidebars.js'),
-          },
-          theme: {
-            customCss: require.resolve('./src/css/custom.css'),
-          },
-        },
-      ],
-    ],
-  };
+    footer : {
+      style : 'dark',
+      copyright : `Copyright © ${
+                    new Date()
+                        .getFullYear()} Wiktor Bajor. Built with Docusaurus.`,
+    },
+  },
+  presets :
+          [
+            [
+              '@docusaurus/preset-classic',
+              {
+                docs : {
+                  sidebarPath : require.resolve('./sidebars.js'),
+                },
+                theme : {
+                  customCss : require.resolve('./src/css/custom.css'),
+                },
+              },
+            ],
+          ],
+};
